6b0e36ff14905e1d5805bdbeba8567f793df076d
MaxFilsR <125319885+MaxFilsR@users.noreply.github.com>
2024-03-07 11:57:45 -0500

    Solution

--- Problems/problem1.py
+++ Problems/problem1.py
@@ -8,3 +8,11 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 Author:
 Max Fils Remfort
 """
+
+sum = 0  # Initialize the result variable
+
+for number in range(1, 1000):
+    if number % 3 == 0 or number % 5 == 0:
+        sum += number
+
+  print(sum)
